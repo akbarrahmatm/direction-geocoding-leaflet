d359cc968e9a221ff8503f8a81da7864ab2b2fb0
akbarrahmatm <akbarrahmatmulyatama@gmail.com>
2023-01-15 20:27:55 +0700

    show user current location

--- maps.js
+++ maps.js
@@ -1,5 +1,7 @@
 var map = L.map('map').setView([-6.200000, 106.816666], 12);
 
+map.locate({setView: true, maxZoom: 16});
+
 L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
@@ -9,3 +11,14 @@ L.Routing.control({
     routeWhileDragging: false,
     geocoder : new L.Control.Geocoder.Nominatim()
 }).addTo(map);
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
